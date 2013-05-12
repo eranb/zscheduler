40121cc89f82906ee95f2f7a94d0723375f2be7c
eranb <barakviz@gmail.com>
2013-05-12 11:32:50 +0300

    cleanup

--- lib/zscheduler.rb
+++ lib/zscheduler.rb
@@ -7,16 +7,14 @@ module Zscheduler
     def every(frequency,options = {}, &block)
       block_given? or raise ArgumentError, "no block was given..."
 
-      reactor_running? or start_reactor
+      EM.reactor_running? or start_reactor
 
       add_shutdown_hook &block if options[:on_shutdown]
       block.call if options[:immediately]
 
-      timers.push EM::PeriodicTimer.new(Integer frequency) do
+      timers.push(EM::PeriodicTimer.new(Integer frequency) do
         options[:on_thread] ? Thread.new(&block) : block.call
-      end
-
-      timers.last
+      end).last
     end
 
     def stop
@@ -38,13 +36,9 @@ module Zscheduler
     private
 
     def start_reactor
-      @wrapper = Thread.new { EventMachine.run }
+      @wrapper = Thread.new &EM.method(:run)
       wrapper.abort_on_exception = true
-      Thread.pass until reactor_running?
-    end
-
-    def reactor_running?
-      EventMachine.reactor_running?
+      Thread.pass until EM.reactor_running?
     end
 
     def wrapper
